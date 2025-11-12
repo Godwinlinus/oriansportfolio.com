97ebe63ff8738759ffb3558b3065442b7d031464
Godwin Orian <linusgodwin2@gmail.com>
2025-11-12 02:49:59 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -65,7 +65,7 @@ export default function Contact() {
       </div>
 
       <motion.a
-        href="tel:09025372950" // this will open the phone dialer
+        href="+2349025372950" // this will open the phone dialer
         whileHover={{ scale: 1.1 }}
         transition={{ type: "spring", stiffness: 250 }}
         className="mt-16 inline-flex items-center gap-3 px-8 py-4 rounded-full bg-gradient-to-r from-purple-500 to-fuchsia-500 text-white font-semibold tracking-widest shadow-lg hover:from-fuchsia-500 hover:to-purple-500 transition-all"
